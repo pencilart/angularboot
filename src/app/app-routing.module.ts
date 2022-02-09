3252e30936f282e46195e408a534df1ce48c70af
Vijay <vijay@Vijays-iMac.local>
2022-02-09 12:07:36 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VenueComponent } from './venue/venue.component';
+import { LocationsComponent } from './locations/locations.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {path: "", pathMatch: "full", redirectTo: "/home"},
+  {path: "home", component: HomeComponent},
+  {path: "venue", component: VenueComponent},
+  {path: "locations", component: LocationsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
